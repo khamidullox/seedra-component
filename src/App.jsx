1ab0ae1abada537c47506ef086b997b13f9263b4
khamidullox <hamidullo22022@gmail.com>
2024-05-27 01:57:51 +0500

    header is ready

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Novbar from "./components/Novbar";
+import HeroProduct from "./components/HeroProduct";
+
+function App() {
+  return (
+    <div className="container m-auto px-16">
+      <Novbar />
+      <HeroProduct />
+    </div>
+  );
+}
+
+export default App;
